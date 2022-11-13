281201bd29ffcfd731c27820f815e1721418fc0b
anhleduc12081995 <anh.leduc@gigantec.media>
2022-11-13 19:52:31 +0700

    trigger lint fail

--- src/app.py
+++ src/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='text-align: center;'>Hello World, my name is Anh Le</h1>"
-
+a
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
